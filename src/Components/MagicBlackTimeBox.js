b9eaa88461564d341e4682ee3e09b9d4ab3cb77c
orjon <github@orjon.com>
2020-06-15 21:24:23 +0100

    Responsiveness to screen orientation implemented

--- src/Components/MagicBlackTimeBox.js
+++ src/Components/MagicBlackTimeBox.js
@@ -9,10 +9,7 @@ class MagicBlackTimeBox extends Component {
     super(props);
     this.state= {
       date: new Date(),
-      theHour: 0,
-      theMins: 0,
-      theSecs: 0,
-      clockFace: [12,5,9]
+      clockFace: [12,5,9,60]
       };
   }
 
@@ -27,6 +24,7 @@ class MagicBlackTimeBox extends Component {
       let clockHour = newDate.getHours()
       let clockDecs = Math.floor((newDate.getMinutes())/10)
       let clockMins = (newDate.getMinutes())%10
+      let clockSecs = newDate.getSeconds()
 
       if (clockHour > 12) {
         clockHour = clockHour-12
@@ -40,12 +38,33 @@ class MagicBlackTimeBox extends Component {
         clockHour: clockHour,
         clockDecs: clockDecs,
         clockMins: clockMins,
+        clockSecs: clockSecs
       })
     },1000);
   }
 
   componentDidMount(){
     this.callMe()
+  }
+
+  checkWindowSize(){
+    var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    console.log(windowHeight);
+    console.log(windowWidth);
+  //   $('#displaySize').text(windowWidth + 'W x ' +  windowHeight + 'H');
+
+   if (windowWidth < windowHeight){
+      document.documentElement.style.setProperty('--clockSize', '73vw');
+      return 'portrait';
+      // clockSize = (0.73 * windowWidth);
+      //  console.log('w:'+ clockSize);
+    } else {
+       document.documentElement.style.setProperty('--clockSize', '73vh');
+       return 'landscape'
+      //  clockSize = (0.73 * windowHeight);
+      // console.log('h: ' +clockSize);
+    }
   }
 
   
@@ -55,6 +74,9 @@ class MagicBlackTimeBox extends Component {
 
     let { clockHour, clockDecs, clockMins, clockFace } = this.state
 
+
+    let orientation = this.checkWindowSize()
+    console.log(orientation)
     
     var hourCircles = [];
     for (let i = 0; i < clockHour; i++) {
@@ -94,13 +116,13 @@ class MagicBlackTimeBox extends Component {
         <div className='Time'>
           {this.state.theHour}:{this.state.theMins}:{this.state.theSecs}
         </div>
-        <div className='ClockFace'>
+        <div className={`ClockFace ${orientation}`}>
           <div className='CircleHours'>{hourCirclesEmpty}</div>
           <div className='CircleDecs'>{decsCirclesEmpty}</div>
           <div className='CircleMins'>{minsCirclesEmpty}</div>
         </div>
 
-        <div className='ClockFace'>
+        <div className={`ClockFace ${orientation}`}>
           <div className='CircleHours'>{hourCircles}</div>
           <div className='CircleDecs'>{decsCircles}</div>
           <div className='CircleMins'>{minsCircles}</div>
